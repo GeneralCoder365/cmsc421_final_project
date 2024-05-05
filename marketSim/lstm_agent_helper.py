97998cf28d0d6301c2692479abc5eb7e3c7a29ac
alec-luterman <amluter@terpmail.umd.edu>
2024-05-04 22:28:49 -0400

    lstm + results

--- marketSim/lstm_agent_helper.py
+++ marketSim/lstm_agent_helper.py
@@ -1,10 +1,12 @@
 import keras
 import lstm_agent as lstm
 import tensorflow as tf
+import pandas as pd
 
 class agent_helper():
 
-    def __init__(self, env, stock_list):
+    # creates agent to make predictions for each stock
+    def __init__(self, env, stock_list, train_test):
 
         self.env = env
         # self.stock_list = stock_list
@@ -12,44 +14,97 @@ class agent_helper():
         self.model_list = []
         self.lstm_dataX = []
         self.lstm_dataY = []
+        self.total_correct_pred_daily = []
+        self.num_days = 0
 
         for name, data in stock_list:
 
+            print('Preparing LSTM for', name)
+            
+            # load all lstm models
             path = 'models/' + name + '_lstm_model.keras'
             curr = keras.models.load_model(path)
             self.model_list.append(curr)
 
-            # getting data to make predictions and compare to actual
+            # preparing data to make predictions and compare to actual
             tmp = lstm.build_table(data)
             tmpx, tmpy = lstm.pre_train(tmp)
-            _, X_test, _, Y_test = lstm.split_data(tmpx, tmpy, 0.9999)
+            _, X_test, _, Y_test = lstm.split_data(tmpx, tmpy, train_test)
             self.lstm_dataX.append(X_test)
             self.lstm_dataY.append(Y_test)
 
-        print(self.lstm_dataX[0].shape)
+        self.total_correct_pred = [0] * len(self.model_list)
         
+    # given a observation index, return the optimal action for each stock
     def action(self, observation_idx):
 
-        threshold = 0.005
+        self.num_days += 1
+
+        correct_pred = 0
+
+        threshold = 0.0000005
 
         action_list = []
 
+        # action for every stock along with analysis
         for idx, model in enumerate(self.model_list):
 
-            # prediction = model.predict(self.lstm_dataX[idx][[observation_idx]]
+            # get prediction
             prediction = model(self.lstm_dataX[idx][[observation_idx]])
             prev_day = self.lstm_dataY[idx][observation_idx - 1]
 
             diff = prediction[0][0] - prev_day[0]
 
-            if(diff > threshold):
+            curr_action = 0
 
-                action_list.append(1) #buy
+            # make decision
+            if(diff > threshold): # buy
+                curr_action = 1
             
-            elif(diff < -1 * threshold):
+            elif(diff < -1 * threshold): # sell
+                curr_action = 2
 
-                action_list.append(2) #sell
-            else:
-                action_list.append(0) #hold
+            action_list.append(curr_action)
+
+            actual_val = self.lstm_dataY[idx][observation_idx]
+            actual_diff = actual_val[0] - prev_day[0]
+
+            # compare prediction to actual result
+            if(actual_diff > threshold): # should buy
+
+                if(curr_action == 1):
+
+                    correct_pred += 1
+                    self.total_correct_pred[idx] += 1
+            
+            elif(actual_diff < -1 * threshold): # should sell
+
+                if(curr_action == 2):
+
+                    correct_pred += 1
+                    self.total_correct_pred[idx] += 1
+
+            else: # should hold
+                
+                if(curr_action == 0):
+
+                    correct_pred += 1
+                    self.total_correct_pred[idx] += 1
+
+            # print(idx)
+            # print(len(self.total_correct_pred))
+            # self.total_correct_pred[idx] += correct_pred
+
+            
+        print('Current correct predictions:', correct_pred)
+        self.total_correct_pred_daily.append(correct_pred)
 
         return action_list
+    
+    def get_total_correct_predictions_daily(self):
+
+        return self.total_correct_pred_daily
+    
+    def get_total_correct_predictions(self):
+
+        return self.total_correct_pred, self.num_days
---
+++ marketSim/lstm_main.py
@@ -0,0 +1,21 @@
+import marketEnv as me
+import lstm_agent_helper as LSTM
+
+environment = me.markEnv(train_test_stop=0.9999)
+train_test = environment.get_train_test_stop()
+lstm = LSTM.agent_helper(environment, environment.get_unmodified_stocks(), train_test)
+
+done = False
+new_state = 0
+
+while not done:
+
+    actions = lstm.action(new_state)
+    new_state, reward, done, _ = environment.step(actions)
+    print(reward, '\n\n')
+
+print('Profits:', environment.get_profits(), '\n\n')
+correct_decisions, num_days = lstm.get_total_correct_predictions()
+print('Correct Decision:', correct_decisions, '\n\n')
+percentage_correct = [x/num_days for x in correct_decisions]
+print('Percent Correct:', percentage_correct)
